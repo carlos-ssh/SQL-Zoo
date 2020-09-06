308973f9c2b9d4bcf28756e496b757f591f2fc0e
Carlos-SSH <carlos.anezeh@me.com>
2020-09-05 20:52:15 -0500

    Create JOIN file:
    Add and create joint.sql here i will to create excersises about the JOIN SQL function from SQLZOO.

--- SELECT_from_WORLD/select-from-world.sql
+++ SELECT_from_WORLD/select-from-world.sql
@@ -1,10 +1,12 @@
 /*Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.*/
 
-SELECT name, continent, population FROM world
+SELECT name, continent, population 
+FROM world
 
 /*How to use WHERE to filter records. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.*/
 
-SELECT name FROM world
+SELECT name 
+FROM world
 WHERE population > 200000000
 
 
