8543812f0b19ded4be5c952457c97a82dbb04ec0
Carlos-SSH <carlos.anezeh@me.com>
2020-09-07 16:16:58 -0500

    create new queries

--- More_JOIN/more_join.sql
+++ More_JOIN/more_join.sql
@@ -9,3 +9,19 @@ SELECT id, title
 SELECT yr
 FROM movie
 WHERE title= 'Citizen Kane'
+
+/*List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.*/
+
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE 'Star Trek%'
+ORDER BY yr
+
+/*What id number does the actor 'Glenn Close' have?*/
+
+SELECT id
+FROM actor
+WHERE name='Glenn Close'
+
+
+/**/
